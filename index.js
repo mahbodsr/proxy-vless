b3d34449ffe06a5bf455bfcacf5b84eddb94d899
mahbodsr <srmahbod@gmail.com>
2024-06-30 17:43:03 +0330

    new vless (open-source)

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const { createVLESSServer } = require("@3kmfi6hp/nodejs-proxy");
+require("./vless");
 require("./job")
 
-const port = 10000;
-const uuid = "e6752ef9-4518-473f-a278-07c098878c56";
+// const port = 10000;
+// const uuid = "e6752ef9-4518-473f-a278-07c098878c56";
 
-createVLESSServer(port, uuid);
+// createVLESSServer(port, uuid);
