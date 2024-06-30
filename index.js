d5e26a4a4f93c7c969d4780dae65502d00a6fb34
mahbodsr <srmahbod@gmail.com>
2024-06-30 19:15:21 +0330

    vless-cf

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const {createVLESSServer} = require("./vlesscf");
-require("./job")
+const { createVLESSServer } = require("./vlesscf.js");
+require("./job");
 
 const port = 10000;
 const uuid = "e6752ef9-4518-473f-a278-07c098878c56";
