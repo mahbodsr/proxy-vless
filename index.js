0fae50fdf71a2beef9ee9bc94c5f8e5d7a4290e9
mahbodsr <srmahbod@gmail.com>
2024-07-01 18:02:27 +0330

    typescript version of vless and open-sourced

--- index.js
+++
@@ -1,8 +0,0 @@
-const { createVLESSServer } = require("@3kmfi6hp/nodejs-proxy");
-// require("./vless.js");
-require("./job.js");
-
-const port = 10000;
-const uuid = "e6752ef9-4518-473f-a278-07c098878c56";
-
-createVLESSServer(port, uuid);
---
+++ vless.ts
@@ -0,0 +1,52 @@
+import net from "net";
+import { WebSocket, createWebSocketStream } from "ws";
+import { TextDecoder } from "util";
+// const logcb = (...args) => console.log.bind(this, ...args);
+// const errcb = (...args) => console.error.bind(this, ...args);
+
+const uuid = "e6752ef9-4518-473f-a278-07c098878c56".replace(/-/g, "");
+const port = 10000;
+
+const wss = new WebSocket.Server({ port });
+wss.on("connection", (ws) => {
+  ws.once("message", (msg) => {
+    if (msg instanceof ArrayBuffer || Array.isArray(msg)) return;
+    const [VERSION] = msg;
+    const id = msg.subarray(1, 17);
+    if (!id.every((v, i) => v == parseInt(uuid.substr(i * 2, 2), 16))) return;
+    let i = msg.subarray(17, 18).readUInt8() + 19;
+    const port = msg.subarray(i, (i += 2)).readUInt16BE(0);
+    const ATYP = msg.subarray(i, (i += 1)).readUInt8();
+    const host =
+      ATYP == 1
+        ? msg.subarray(i, (i += 4)).join(".") //IPV4
+        : ATYP == 2
+        ? new TextDecoder().decode(
+            msg.subarray(i + 1, (i += 1 + msg.subarray(i, i + 1).readUInt8()))
+          ) //domain
+        : ATYP == 3
+        ? msg
+            .subarray(i, (i += 16))
+            .reduce(
+              // @ts-ignore
+              (s, b, i, a) => (i % 2 ? s.concat(a.slice(i - 1, i + 1)) : s),
+              []
+            )
+            // @ts-ignore
+            .map((b) => b.readUInt16BE(0).toString(16))
+            .join(":")
+        : ""; //ipv6
+
+    ws.send(new Uint8Array([VERSION, 0]));
+    const duplex = createWebSocketStream(ws);
+    const socket = net.connect({ host, port }, () => {
+      socket.write(msg.subarray(i));
+      duplex
+        .on("error", () => {})
+        .pipe(socket)
+        .on("error", () => {})
+        .pipe(duplex);
+    });
+    socket.on("error", () => {});
+  }).on("error", () => {});
+});
