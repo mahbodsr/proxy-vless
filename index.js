8aaa81c4868276de8da81352f19f368aea4e7148
mahbodsr <srmahbod@gmail.com>
2024-07-01 11:35:02 +0330

    using library

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const { createVLESSServer } = require("./vlesscf.js");
-require("./job");
+const { createVLESSServer } = require("@3kmfi6hp/nodejs-proxy");
+// require("./vless.js");
+require("./job.js");
 
 const port = 10000;
 const uuid = "e6752ef9-4518-473f-a278-07c098878c56";
